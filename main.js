597b4d217915b6aac76dae58535ad4d26d10a08b
Mr. Huniko <lovelysong1205@gmail.com>
2023-06-08 16:16:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ Copyright (c) 2023-present [Huniko519](https://github.com/Huniko519)
       repo: repository.split("/")[1],
       path: "README.md"
     });
-//     console.log(sha);
+    console.log(sha);
     
     const content = before + middle + end;
     await octokit.repos.createOrUpdateFileContents({
